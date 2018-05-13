4726f063e659e7e89f2785c640776bf7f3ef53d4
S-Shimotori <490.frostybirds@gmail.com>
2018-05-13 14:25:37 +0900

    Update Neo4j example

--- graph/src/main/java/net/terminal_end/graph/Example.java
+++ graph/src/main/java/net/terminal_end/graph/Example.java
@@ -2,8 +2,9 @@ package net.terminal_end.graph;
 
 
 import net.terminal_end.graph.domain.Clazz;
+import net.terminal_end.graph.domain.ConformsTo;
+import net.terminal_end.graph.domain.Protocol;
 import net.terminal_end.graph.service.ClazzService;
-import org.neo4j.ogm.session.Session;
 
 import java.util.Calendar;
 import java.util.HashSet;
@@ -14,11 +15,22 @@ public class Example {
 
         String testName = Calendar.getInstance().getTime().toString();
         Clazz clazz = Clazz.builder()
-                           .name(testName)
-                           .conformsToSet(new HashSet<>())
+                           .name("class(" + testName + ")")
                            .build();
+        Protocol protocol = Protocol.builder()
+                                    .name("protocol(" + testName + ")")
+                                    .build();
+        ConformsTo conformsTo = ConformsTo.builder()
+                                          .abstractType(protocol)
+                                          .concreteType(clazz)
+                                          .build();
+        HashSet<ConformsTo> conformsToSet = new HashSet<>();
+        conformsToSet.add(conformsTo);
+        clazz.setConformsToSet(conformsToSet);
+        protocol.setConformsToSet(conformsToSet);
 
         new ClazzService().createOrUpdate(clazz);
         System.exit(0);
+
     }
 }
